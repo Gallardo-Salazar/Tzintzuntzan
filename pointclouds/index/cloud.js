3a0a31f4a38bbab08486fb759707dac434017d9c
unknown <gallardoirg@gmail.com>
2022-03-27 00:46:16 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,115 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 115486871,
+    "boundingBox": {
+        "lx": 229685.25,
+        "ly": 2171197.296875,
+        "lz": 2058.2470703125,
+        "ux": 230742.296875,
+        "uy": 2172254.34375,
+        "uz": 3115.2939453125
+    },
+    "tightBoundingBox": {
+        "lx": 229685.25,
+        "ly": 2171197.297,
+        "lz": 2058.247,
+        "ux": 230687.047,
+        "uy": 2172254.344,
+        "uz": 2186.215
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "Normal",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "Group",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "Distance",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 9.15429401397705,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
